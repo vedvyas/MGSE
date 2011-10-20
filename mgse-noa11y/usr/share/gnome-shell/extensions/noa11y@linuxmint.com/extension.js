8801adef2556150042d38778d115fe956fb25764
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2011-10-20 17:16:41 +0100

    Fixed bug in noa11y

--- mgse-noa11y/usr/share/gnome-shell/extensions/noa11y@linuxmint.com/extension.js
+++ mgse-noa11y/usr/share/gnome-shell/extensions/noa11y@linuxmint.com/extension.js
@@ -4,27 +4,26 @@ const Panel = imports.ui.panel;
 idx = null;
 
 function init(extensionMeta) {
-    
+    indicator = new Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"];
 }
 
 function enable() {
     // Remove A11Y menu     
-    //for (let i = 0; i < Main.panel._rightBox.get_children().length; i++) {
-    //    if (Main.panel._statusArea['a11y'] == Main.panel._rightBox.get_children()[i]._delegate) {
-    //        Main.panel._rightBox.get_children()[i].destroy();
-    //        break;
-    //    }
-    //}
+    for (let i = 0; i < Main.panel._rightBox.get_children().length; i++) {
+        if (Main.panel._statusArea['a11y'] == Main.panel._rightBox.get_children()[i]._delegate) {
+            Main.panel._rightBox.get_children()[i].destroy();
+            break;
+        }
+    }
     // addToStatusArea would throw an error on disable if we don't set this to null
-    //Main.panel._statusArea['a11y'] = null;    
+    Main.panel._statusArea['a11y'] = null;    
     
-    idx = Panel.STANDARD_STATUS_AREA_ORDER.indexOf("a11y");
-	Main.panel._rightBox.get_children()[idx].destroy();
+    //idx = Panel.STANDARD_STATUS_AREA_ORDER.indexOf("a11y");
+	//Main.panel._rightBox.get_children()[idx].destroy();
 	// addToStatusArea would throw an error on disable if we don't set this to null 
-	Main.panel._statusArea['a11y'] = null;
+	//Main.panel._statusArea['a11y'] = null;    
 }
 
 function disable() {
-   let indicator = new Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"];
    Main.panel.addToStatusArea("a11y", indicator, idx);
 }
